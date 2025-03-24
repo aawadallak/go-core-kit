8c36ce76f8b88904c8d06a9799c30f78940af109
alexandre.awadallak <alexandre.awadallak@gmail.com>
2025-03-24 10:33:37 +0400

    Updated go docs

--- core/conf/opts.go
+++ core/conf/opts.go
@@ -1,26 +1,50 @@
 package conf
 
-// Option is a function type that modifies a config instance.
+// Option represents a function that modifies a configuration instance.
+// It takes a pointer to a valueMap and applies specific configuration changes.
 type Option func(*valueMap)
 
-// WithProvider adds a new provider to the configuration.
-// Returns an Option function that can be used to modify a config instance.
+// WithProvider creates an Option that adds a new provider to the configuration.
+// The provider will be appended to the existing list of providers used for
+// retrieving configuration values.
+//
+// Parameters:
+//
+//	provider - The Provider implementation to add to the configuration
+//
+// Returns:
+//
+//	An Option function that modifies a config instance by adding the provider
 func WithProvider(provider Provider) Option {
 	return func(c *valueMap) {
 		c.providers = append(c.providers, provider)
 	}
 }
 
-// With
+// WithMustLoad creates an Option that sets the configuration to require
+// successful loading of values. When enabled, the configuration will panic
+// if values cannot be loaded from any provider.
+//
+// Returns:
+//
+//	An Option function that enables the mustLoad flag on a config instance
 func WithMustLoad() Option {
 	return func(c *valueMap) {
 		c.mustLoad = true
 	}
 }
 
-// newConfig creates a new config instance with the provided options.
-// It initializes with a default provider and applies any additional options.
-// Returns a pointer to the new config instance.
+// newConfig initializes and returns a new configuration instance with the
+// specified options. The configuration starts with a default environment
+// provider and applies all provided options in the order they are given.
+//
+// Parameters:
+//
+//	opts - Variable number of Option functions to apply to the configuration
+//
+// Returns:
+//
+//	A pointer to the newly created and configured valueMap instance
 func newConfig(opts ...Option) *valueMap {
 	c := &valueMap{
 		providers: []Provider{newEnvProvider()},
